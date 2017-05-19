139a176a2cf8ec665a02807b69045837242105b0
Yamid Pico Leal <ypicoleal@gmail.com>
2017-05-18 20:34:38 -0500

    Primera vista de chat

--- app/src/main/java/co/com/exile/exile/MainActivity.java
+++ app/src/main/java/co/com/exile/exile/MainActivity.java
@@ -2,11 +2,15 @@ package co.com.exile.exile;
 
 import android.os.Bundle;
 import android.support.annotation.IdRes;
-import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 
@@ -14,19 +18,22 @@ import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.BottomBarTab;
 import com.roughike.bottombar.OnTabSelectListener;
 
+import co.com.exile.exile.chat.ChatFragment;
 import co.com.exile.exile.task.TasksFragmetPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
     TasksFragmetPagerAdapter tasksAdapter;
     private ViewPager mViewPager;
-    private FloatingActionButton fab;
     private FrameLayout fragmentContainer;
     private TabLayout tabs;
+    private AppBarLayout appBar;
+    private Menu menu;
 
     private OnTabSelectListener tabSelectListener = new OnTabSelectListener() {
         @Override
         public void onTabSelected(@IdRes int tabId) {
+            appBar.setExpanded(true);
             switch (tabId) {
                 case R.id.navigation_tasks:
                     mViewPager.setVisibility(View.VISIBLE);
@@ -35,24 +42,38 @@ public class MainActivity extends AppCompatActivity {
                     }
                     mViewPager.setAdapter(tasksAdapter);
                     fragmentContainer.setVisibility(View.GONE);
-                    fab.hide();
+                    hideOption(R.id.nav_add);
                     tabs.setVisibility(View.VISIBLE);
                     break;
                 case R.id.navigation_report:
-                    fragmentContainer.setVisibility(View.VISIBLE);
-                    mViewPager.setVisibility(View.GONE);
-                    fab.show();
-                    tabs.setVisibility(View.GONE);
+                    showFragment(-1);
                     break;
                 case R.id.navigation_chat:
-                    fragmentContainer.setVisibility(View.VISIBLE);
-                    mViewPager.setVisibility(View.GONE);
-                    fab.show();
-                    tabs.setVisibility(View.GONE);
+                    showFragment(R.id.navigation_chat);
                     break;
             }
         }
     };
+
+    void showFragment(int fragment) {
+        fragmentContainer.setVisibility(View.VISIBLE);
+        mViewPager.setVisibility(View.GONE);
+        showOption(R.id.nav_add);
+        tabs.setVisibility(View.GONE);
+
+        Fragment f;
+
+        if (fragment == R.id.navigation_chat) {
+            f = new ChatFragment();
+        } else {
+            f = new ChatFragment();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, f)
+                .commit();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,11 +83,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        appBar = (AppBarLayout) findViewById(R.id.appbar);
         mViewPager = (ViewPager) findViewById(R.id.pager);
         tabs = (TabLayout) findViewById(R.id.tabs);
         tabs.setupWithViewPager(mViewPager);
 
-        fab = (FloatingActionButton) findViewById(R.id.fab);
         fragmentContainer = (FrameLayout) findViewById(R.id.fragment_container);
 
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
@@ -77,4 +98,24 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.menu = menu;
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    private void hideOption(int id) {
+        if (menu != null) {
+            MenuItem item = menu.findItem(id);
+            item.setVisible(false);
+        }
+    }
+
+    private void showOption(int id) {
+        if (menu != null) {
+            MenuItem item = menu.findItem(id);
+            item.setVisible(true);
+        }
+    }
 }
