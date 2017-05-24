fb3408da23fa2dfda52c821638d303a4859b69b0
Yamid Pico Leal <ypicoleal@gmail.com>
2017-05-24 18:33:46 -0500

    implementacion de logica para login #1

--- app/src/main/java/co/com/exile/exile/LoginActivity.java
+++ app/src/main/java/co/com/exile/exile/LoginActivity.java
@@ -1,13 +1,92 @@
 package co.com.exile.exile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.v4.widget.ContentLoadingProgressBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import co.com.exile.exile.network.VolleySingleton;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+        TextInputEditText username = (TextInputEditText) findViewById(R.id.username);
+        TextInputEditText password = (TextInputEditText) findViewById(R.id.password);
+        login(username.getText().toString(), password.getText().toString());
+    }
+
+    public void login(final String username, final String password) {
+        String serviceUrl = getString(R.string.login);
+
+        String url = getString(R.string.url, serviceUrl);
+        StringRequest loginRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        saveAccount(username, password);
+                        hideLoading();
+                        initMain();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        hideLoading();
+                        if (error.networkResponse != null && (error.networkResponse.statusCode == 404 || error.networkResponse.statusCode == 400)) {
+                            Snackbar.make(findViewById(R.id.main_container), "usuario y/o contraseña incorrecta", 800).show();
+                        } else {
+                            Snackbar.make(findViewById(R.id.main_container), "Error al hacer la consulta", 800).show();
+                        }
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+                return params;
+            }
+        };
+        loginRequest.setRetryPolicy(new DefaultRetryPolicy(0, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        VolleySingleton.getInstance(this).addToRequestQueue(loginRequest);
+        showLoading();
+    }
+
+    private void showLoading() {
+        findViewById(R.id.login_ly).setVisibility(View.INVISIBLE);
+        ((ContentLoadingProgressBar) findViewById(R.id.loading)).show();
+    }
+
+    private void hideLoading() {
+        findViewById(R.id.login_ly).setVisibility(View.VISIBLE);
+        ((ContentLoadingProgressBar) findViewById(R.id.loading)).hide();
+    }
+
+    private void initMain() {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    private void saveAccount(String username, String password) {
+
     }
 }
---
+++ app/src/main/java/co/com/exile/exile/network/VolleySingleton.java
@@ -0,0 +1,84 @@
+package co.com.exile.exile.network;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.HttpCookie;
+import java.net.URI;
+import java.util.List;
+
+
+public class VolleySingleton {
+    private static VolleySingleton mInstance;
+    private static Context mCtx;
+    private RequestQueue mRequestQueue;
+    private CookieManager cookieManager;
+
+    private VolleySingleton(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+    }
+
+    public static synchronized VolleySingleton getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new VolleySingleton(context);
+        }
+        return mInstance;
+    }
+
+    private RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            cookieManager = new CookieManager();
+            CookieHandler.setDefault(cookieManager);
+
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    void cancelAll() {
+        mRequestQueue.cancelAll(new RequestQueue.RequestFilter() {
+            @Override
+            public boolean apply(Request<?> request) {
+                Log.d("DEBUG", "request running: " + request.getUrl());
+                return true;
+            }
+        });
+    }
+
+    String getCookie(String name, URI url) {
+        List<HttpCookie> cookies = cookieManager.getCookieStore().get(url);
+        for (HttpCookie cookie : cookies) {
+            if (cookie.getName().equals(name)) {
+                return cookie.getValue();
+            }
+        }
+        return null;
+    }
+
+    /*public static void manageError(Context context, VolleyError error, ViewGroup container, View.OnClickListener retryListener) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View errorView = inflater.inflate(R.layout.no_network, container, false);
+        container.addView(errorView);
+        Button retry = (Button) errorView.findViewById(R.id.retry);
+        if (error.networkResponse == null) {
+            Log.i("retry", "hola " + retry.isClickable());
+            retry.setOnClickListener(retryListener);
+        } else {
+            TextView errorTV = (TextView) errorView.findViewById(R.id.error_text);
+            errorTV.setText(context.getString(R.string.server_error, error.networkResponse.statusCode));
+            retry.setVisibility(View.GONE);
+            Log.i("network", "" + error.networkResponse.statusCode);
+        }
+    }*/
+}
