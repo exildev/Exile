9b85f7a327cce38e36725579163604801613e309
Yamid Pico <Yamid.X.Pico.-ND@disney.com>
2018-10-18 00:26:45 -0500

    pantalla de calendario

--- app/src/main/java/co/com/exile/exile/task/ScheduleFragment.java
+++ app/src/main/java/co/com/exile/exile/task/ScheduleFragment.java
@@ -4,6 +4,9 @@ package co.com.exile.exile.task;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +21,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 import co.com.exile.exile.BaseFragment;
 import co.com.exile.exile.R;
 import co.com.exile.exile.network.VolleySingleton;
@@ -29,6 +36,9 @@ import co.com.exile.exile.network.VolleySingleton;
  * create an instance of this fragment.
  */
 public class ScheduleFragment extends BaseFragment {
+
+    ScheduleListAdapter adapter = new ScheduleListAdapter();
+    SwipeRefreshLayout mSwipe;
 
 
     public ScheduleFragment() {
@@ -51,32 +61,58 @@ public class ScheduleFragment extends BaseFragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        mSwipe = view.findViewById(R.id.swipe);
+        mSwipe.setRefreshing(true);
+
+        RecyclerView reportList = view.findViewById(R.id.task_list);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        reportList.setLayoutManager(layoutManager);
+        reportList.setHasFixedSize(false);
+        reportList.setAdapter(adapter);
+
         loadData();
+
+        mSwipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadData();
+            }
+        });
     }
 
     private void loadData() {
 
-        String serviceUrl = getString(R.string.schedule_task_url);
+        Calendar start_date = Calendar.getInstance();
+        start_date.add(Calendar.DAY_OF_MONTH, 1);
+
+        Calendar end_date = Calendar.getInstance();
+        end_date.add(Calendar.DAY_OF_MONTH, 31);
+
+        SimpleDateFormat formatter = new SimpleDateFormat( "yyyy-MM-dd", Locale.getDefault());
+
+        String serviceUrl = getString(R.string.schedule_task_url, formatter.format(start_date.getTime()), formatter.format(end_date.getTime()));
 
         String url = getUrl(serviceUrl);
+
+        Log.e("tales6", url);
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
 
                 try {
                     JSONArray object_list = response.getJSONArray("object_list");
-                    Log.e("tales6", object_list.toString());
-                    //mAdapter.setTasks(object_list);
+                    adapter.setTasks(object_list);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                //mSwipe.setRefreshing(false);
+                mSwipe.setRefreshing(false);
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Log.i("response", error.toString());
-                //mSwipe.setRefreshing(false);
+                mSwipe.setRefreshing(false);
             }
         });
         VolleySingleton.getInstance(this.getContext()).addToRequestQueue(request);
---
+++ app/src/main/java/co/com/exile/exile/task/ScheduleListAdapter.java
@@ -0,0 +1,80 @@
+package co.com.exile.exile.task;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import co.com.exile.exile.R;
+
+public class ScheduleListAdapter extends RecyclerView.Adapter<ScheduleListAdapter.TaskViewHolder> {
+
+    private JSONArray tasks;
+
+    void setTasks(JSONArray tasks) {
+        this.tasks = tasks;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public TaskViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.schedule_task, parent, false);
+        return new ScheduleListAdapter.TaskViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(TaskViewHolder holder, int position) {
+        try {
+            JSONObject item = tasks.getJSONObject(position);
+
+            Log.e("tales5", item.getString("fecha"));
+
+            SimpleDateFormat parser = new SimpleDateFormat("MM/dd/yyyy", Locale.getDefault());
+            SimpleDateFormat formatter = new SimpleDateFormat("MMM dd", Locale.getDefault());
+            Date date = parser.parse(item.getString("fecha"));
+
+            holder.date.setText(formatter.format(date));
+            holder.title.setText(item.getString("title"));
+            holder.description.setText(item.getString("descripcion"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (tasks == null) {
+            return 0;
+        }
+        return tasks.length();
+    }
+
+    class TaskViewHolder extends RecyclerView.ViewHolder {
+
+        TextView date;
+        TextView title;
+        TextView description;
+
+        TaskViewHolder(View itemView) {
+            super(itemView);
+
+            date = itemView.findViewById(R.id.date);
+            title = itemView.findViewById(R.id.title);
+            description = itemView.findViewById(R.id.description);
+        }
+    }
+}
